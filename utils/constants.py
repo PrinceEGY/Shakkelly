1bccff4619db173bd19b1b2b5441851587905d48
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-30 19:08:13 +0300

    removed sets to ensure ordering

--- utils/constants.py
+++ utils/constants.py
@@ -8,7 +8,7 @@ KASRATAN = "\u064d"
 SHADDA = "\u0651"
 SUKUN = "\u0652"
 
-HARAKAT = set([FATHATAN, DAMMATAN, KASRATAN, FATHA, DAMMA, KASRA, SHADDA, SUKUN])
+HARAKAT = [FATHATAN, DAMMATAN, KASRATAN, FATHA, DAMMA, KASRA, SHADDA, SUKUN]
 
 # Arabic letters
 AR_LETTERS = "ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنهوىي"
@@ -17,17 +17,19 @@ AR_LETTERS = "ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنه
 PUNCTUATIONS = [".", "،", ":", "؛", "-", "؟", ","]
 
 # Valid Arabic characters
-VALID_ARABIC = HARAKAT.union(set(list(AR_LETTERS) + PUNCTUATIONS))
+VALID_ARABIC = HARAKAT + list(AR_LETTERS) + PUNCTUATIONS
+
 
 def get_diac_vocabulary():
-        vocab = ["", "[UNK]", " "]
-        vocab += list(HARAKAT)
-        
-        tashkeel = HARAKAT - set([SHADDA, SUKUN])
-        for diac in tashkeel:
-            vocab.append(SHADDA+diac)
-    
-        return vocab
+    vocab = ["", "[UNK]", " "]
+    vocab += HARAKAT
+
+    tashkeel = set(HARAKAT) - set([SHADDA, SUKUN])
+    for diac in tashkeel:
+        vocab.append(SHADDA + diac)
+
+    return vocab
+
 
 def get_letters_vocabulary():
     vocab = ["", "[UNK]", " ", "s", "e"]
