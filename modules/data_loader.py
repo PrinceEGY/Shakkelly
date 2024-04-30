0c95332693533ae85769344319d40dfca6df3675
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-30 17:11:39 +0300

    add prefetching to dataset processing

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from utils.preprocessor import Preprocessor
 
+
 class DataLoader:
     def __init__(self, letter_tokenizer, diac_tokenizer):
         self.letter_tokenizer = letter_tokenizer
@@ -15,23 +16,28 @@ class DataLoader:
 
     def process_ds(self, ds, batch_size=32, shuffle_buffer=1000):
         ds = (
-        ds.map(self.tf_strip_tashkeel, tf.data.AUTOTUNE)
-        .map(lambda x, y: (self.letter_tokenizer(x), self.diac_tokenizer(y)), tf.data.AUTOTUNE)
-        .shuffle(shuffle_buffer)
-        .padded_batch(batch_size)
+            ds.map(self.tf_strip_tashkeel, tf.data.AUTOTUNE)
+            .map(
+                lambda x, y: (self.letter_tokenizer(x), self.diac_tokenizer(y)),
+                tf.data.AUTOTUNE,
+            )
+            .shuffle(shuffle_buffer)
+            .padded_batch(batch_size)
+            .prefetch(tf.data.AUTOTUNE)
         )
-    
+
         return ds
 
     def tf_strip_tashkeel(self, inputs):
         @tf.py_function(Tout=(tf.string, tf.string))
         def strip_tashkeel(inputs):
-            text = inputs.numpy().decode('utf-8')
+            text = inputs.numpy().decode("utf-8")
             text, tashkeel = Preprocessor.strip_tashkeel(text)
             text = tf.convert_to_tensor(text, dtype=tf.string)
             tashkeel = tf.convert_to_tensor(tashkeel, dtype=tf.string)
             return text, tashkeel
+
         text, tashkeel = strip_tashkeel(inputs)
-        text.set_shape((None, ))
-        tashkeel.set_shape((None, ))
+        text.set_shape((None,))
+        tashkeel.set_shape((None,))
         return text, tashkeel
