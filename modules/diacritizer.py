08300c5eb1b362aeec0b1c108f6980c921abd8d4
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-30 17:12:14 +0300

    add model arg to use in inference

--- modules/diacritizer.py
+++ modules/diacritizer.py
@@ -1,21 +1,41 @@
 import keras
 
+
 class Diacritizer:
-    def __init__(self, letters_tokenizer, diacritics_tokenizer):
-        self.letters_w2i = keras.layers.StringLookup(vocabulary=letters_tokenizer.get_vocabulary(), mask_token='')
-        self.letters_i2w = keras.layers.StringLookup(vocabulary=letters_tokenizer.get_vocabulary(), mask_token='', invert=True)
-        
-        self.diacritics_w2i = keras.layers.StringLookup(vocabulary=diacritics_tokenizer.get_vocabulary(), mask_token='')
-        self.diacritics_i2w = keras.layers.StringLookup(vocabulary=diacritics_tokenizer.get_vocabulary(), mask_token='', invert=True)
-        
+    def __init__(
+        self,
+        model,
+        letters_tokenizer,
+        diacritics_tokenizer,
+    ):
+        self.model = model
+        self.letters_w2i = keras.layers.StringLookup(
+            vocabulary=letters_tokenizer.get_vocabulary(), mask_token=""
+        )
+        self.letters_i2w = keras.layers.StringLookup(
+            vocabulary=letters_tokenizer.get_vocabulary(), mask_token="", invert=True
+        )
+
+        self.diacritics_w2i = keras.layers.StringLookup(
+            vocabulary=diacritics_tokenizer.get_vocabulary(), mask_token=""
+        )
+        self.diacritics_i2w = keras.layers.StringLookup(
+            vocabulary=diacritics_tokenizer.get_vocabulary(), mask_token="", invert=True
+        )
+
     def diacritize(self, sentence):
         pass
-        
-        
+
     def decode_sentences(self, sentences):
         # work on batch size only
-        return [[char.decode('utf-8') for char in l.numpy()] for l in self.letters_i2w(sentences)]
+        return [
+            [char.decode("utf-8") for char in l.numpy()]
+            for l in self.letters_i2w(sentences)
+        ]
 
     def decode_diacritics(self, diacritics):
         # work on batch size only
-        return [[char.decode('utf-8') for char in d.numpy()] for d in self.diacritics_i2w(diacritics)]
+        return [
+            [char.decode("utf-8") for char in d.numpy()]
+            for d in self.diacritics_i2w(diacritics)
+        ]
