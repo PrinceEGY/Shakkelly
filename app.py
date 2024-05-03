9fff40029dbedc8c6ba1cdefa32c23b0de2bd4d6
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-05-03 04:26:17 +0300

    Refactor diacritizer instantiation

--- app.py
+++ app.py
@@ -4,34 +4,15 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 from flask import Flask, request
 from flask_restful import Resource, Api
-import keras
 import tensorflow as tf
 
 tf.get_logger().setLevel("ERROR")
 from modules.diacritizer import Diacritizer
-from utils import constants
 
 app = Flask(__name__)
 api = Api(app)
 
-letters_tok = keras.layers.TextVectorization(
-    ragged=True,
-    standardize=lambda x: tf.concat([["s"], x, ["e"]], axis=-1),
-    split=None,
-    vocabulary=constants.get_letters_vocabulary(),
-)
-
-diac_tok = keras.layers.TextVectorization(
-    standardize=lambda x: tf.concat([[" "], x, [" "]], axis=-1),
-    ragged=True,
-    split=None,
-    vocabulary=constants.get_diac_vocabulary(),
-)
-diacritizer = Diacritizer(
-    servant="./servants/lstm-emb128-2rnn128-1dense256-v3",
-    letters_tokenizer=letters_tok,
-    diacritics_tokenizer=diac_tok,
-)
+diacritizer = Diacritizer()
 
 
 class Shakkel(Resource):
